f8ae894392aae1afd065d2428f40886feba967ae
杜仁浩 <ak514250@126.com>
2019-06-29 23:44:21 +0800

    增加测试工程

---
+++ gateway-client-sample/src/main/java/com/weweibuy/gateway/client/sample/SampleApplication.java
@@ -0,0 +1,18 @@
+package com.weweibuy.gateway.client.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author durenhao
+ * @date 2019/6/29 22:51
+ **/
+@SpringBootApplication
+public class SampleApplication {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(SampleApplication.class, args);
+    }
+
+}
