c1db771bd845dd5b6f0303f4bec49bd49471c16f
杜仁浩 <ak514250@126.com>
2020-02-23 10:14:32 +0800

    更改 pom

--- gateway-server/src/main/java/com/weweibuy/gateway/server/GatewayApplication.java
+++ gateway-server/src/main/java/com/weweibuy/gateway/server/GatewayApplication.java
@@ -1,19 +1,15 @@
 package com.weweibuy.gateway.server;
 
-import com.ctrip.framework.apollo.spring.annotation.EnableApolloConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author durenhao
  * @date 2019/5/12 22:38
  **/
 @SpringBootApplication
-@EnableEurekaClient
 @EnableDiscoveryClient
-@EnableApolloConfig
 public class GatewayApplication {
 
     public static void main(String[] args) {
