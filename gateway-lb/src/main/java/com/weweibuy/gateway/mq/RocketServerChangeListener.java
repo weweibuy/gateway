d33c1f97b04941a9a72510c9d811cc904ef0748d
杜仁浩 <ak514250@126.com>
2020-03-09 21:37:17 +0800

    更改 lb 通过接受MQ消息 快速更新 本地服务列表

--- gateway-lb/src/main/java/com/weweibuy/gateway/mq/RocketServerChangeListener.java
+++ gateway-lb/src/main/java/com/weweibuy/gateway/mq/RocketServerChangeListener.java
@@ -2,39 +2,41 @@ package com.weweibuy.gateway.mq;
 
 import com.netflix.loadbalancer.DynamicServerListLoadBalancer;
 import com.netflix.loadbalancer.ILoadBalancer;
+import com.netflix.loadbalancer.Server;
 import com.weweibuy.framework.rocketmq.annotation.Payload;
 import com.weweibuy.framework.rocketmq.annotation.RocketConsumerHandler;
 import com.weweibuy.framework.rocketmq.annotation.RocketListener;
 import com.weweibuy.gateway.mq.message.ServerChangeMessage;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.rocketmq.common.protocol.heartbeat.MessageModel;
-import org.springframework.beans.BeansException;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.netflix.ribbon.SpringClientFactory;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 /**
  * @author durenhao
  * @date 2020/3/7 20:42
  **/
 @Slf4j
-//@Component
+@Component
 @RocketListener(topic = "SERVER_CHANGE_TOPIC", group = "SERVER_CHANGE_GW_C_GROUP", messageModel = MessageModel.BROADCASTING, threadMax = 1, threadMin = 1)
-public class RocketServerChangeListener implements ApplicationContextAware {
+public class RocketServerChangeListener  {
 
-    private ILoadBalancer loadBalancer;
+    @Autowired
+    private SpringClientFactory springClientFactory;
 
     @RocketConsumerHandler
     public void update(@Payload ServerChangeMessage changeMessage) {
         log.info("接受到服务变更消息");
+        ILoadBalancer loadBalancer = springClientFactory.getLoadBalancer(changeMessage.getName());
+        List<Server> allServers = loadBalancer.getAllServers();
+        List<Server> reachableServers = loadBalancer.getReachableServers();
         if (loadBalancer instanceof DynamicServerListLoadBalancer) {
-            DynamicServerListLoadBalancer dynamicServerListLoadBalancer = (DynamicServerListLoadBalancer) this.loadBalancer;
+            DynamicServerListLoadBalancer dynamicServerListLoadBalancer = (DynamicServerListLoadBalancer) loadBalancer;
             dynamicServerListLoadBalancer.updateListOfServers();
         }
     }
 
-    @Override
-    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-        ILoadBalancer bean = applicationContext.getBean(ILoadBalancer.class);
-        this.loadBalancer = bean;
-    }
 }
